ee2fa4409219bef0db71932f53a6279a6ff798d8
Dipesh-Raj-S <dipeshraj.suresh@gmail.com>
2026-06-13 19:28:39 +0530

    built flask-smorest REST API, just before blueprint

---
+++ db.py
@@ -0,0 +1,42 @@
+stores={}
+items={}
+
+
+"""
+Final Memory Structure
+
+stores
+stores = {
+    "abc123": {
+        "name": "My Store",
+        "id": "abc123"
+    },
+
+    "def456": {
+        "name": "Ice Cream Shop",
+        "id": "def456"
+    }
+}
+items
+items = {
+    "xyz999": {
+        "name": "Chair",
+        "price": 17.99,
+        "store_id": "abc123",
+        "id": "xyz999"
+    },
+
+    "pqr888": {
+        "name": "Table",
+        "price": 180.50,
+        "store_id": "abc123",
+        "id": "pqr888"
+    },
+
+    "lmn777": {
+        "name": "Vanilla Ice Cream",
+        "price": 50,
+        "store_id": "def456",
+        "id": "lmn777"
+    }
+}"""
