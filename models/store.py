ad110acb466d8a8a1373ef05aa1613faee9b1c3a
Dipesh-Raj-S <dipeshraj.suresh@gmail.com>
2026-06-15 13:48:37 +0530

    implemented CASCADE DELETION of items,store data  by sqlAlchemy

--- models/store.py
+++ models/store.py
@@ -5,7 +5,7 @@ class StoreModel(db.Model):
 
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(80),unique=True,nullable=False)
-    items=db.relationship("ItemModel",back_populates="store",lazy="dynamic") #in notes.txt
+    items=db.relationship("ItemModel",back_populates="store",lazy="dynamic",cascade="all, delete") #in notes.txt
                                                                             #without lazy dynamic-> All data comes from DB immediately.
                                                                             #when we use it ,SQLAlchemy does NOT fetch items yet.Instead it returns a query object.
                                                                             #"Here is a query.Tell me what you want to do with it."
